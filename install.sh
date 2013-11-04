5b5afd885a874e5cf3f7eab7d725ed8cea5983f5
James Cumming <texel32.doc.ic.ac.uk>
2013-11-04 16:22:43 +0000

    Updated pre commit and post merge

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ echo "Installing all requirements..."
 pip install -r requirements.txt
 echo "Linking post commit and merge hooks..."
 cd .git/hooks
-ln -s ../../hooks/post-commit post-commit
+ln -s ../../hooks/pre-commit pre-commit
 ln -s ../../hooks/post-merge post-merge
 echo "Finished, activate the virtual env with 'source venv/bin/activate' or 'source venv/bin/activate.csh'"
