e19e26307541b5059faca4045b7af2d047037e52
Nigel Gott <nigel.gott@gmail.com>
2013-11-04 13:50:44 +0000

    Fixed bug in install script when setting up git hooks.

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ source venv/bin/activate
 echo "Installing all requirements..."
 pip install -r requirements.txt
 echo "Linking post commit and merge hooks..."
-ln -s post-commit .git/hooks/post-commit
-ln -s post-merge .git/hooks/post-merge
+cd .git/hooks
+ln -s ../../post-commit post-commit
+ln -s ../../post-merge post-merge
