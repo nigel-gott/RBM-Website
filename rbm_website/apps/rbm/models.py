7e5fcd7d4881d4e0cd54cafbbe63a3b879c4ed67
Nigel Gott <nigel.gott@gmail.com>
2013-11-04 22:38:02 +0000

    Added super basic RBM model.

--- rbm_website/apps/rbm/models.py
+++ rbm_website/apps/rbm/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from jsonfield import JSONField
 
 # Create your models here.
+class RBM(models.Model):
+    name = models.CharField(max_length=200)
+    matrix = JSONField()
+    
