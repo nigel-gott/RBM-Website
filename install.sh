2efa187d3eeeb8d959ca8121f31d67114a76008e
Nigel Gott <nigel.gott@gmail.com>
2013-11-04 14:20:46 +0000

    Moved hooks into hooks folder, updated install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ echo "Installing all requirements..."
 pip install -r requirements.txt
 echo "Linking post commit and merge hooks..."
 cd .git/hooks
-ln -s ../../post-commit post-commit
-ln -s ../../post-merge post-merge
+ln -s ../../hooks/post-commit post-commit
+ln -s ../../hooks/post-merge post-merge
+echo "Finished, activate the virtual env with 'source venv/bin/activate' or 'source venv/bin/activate.csh'"
