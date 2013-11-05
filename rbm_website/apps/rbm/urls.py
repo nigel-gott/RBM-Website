87d5b38a4b01e0e51404fa3748f146ec88dcafb5
Nigel Gott <nigel.gott@gmail.com>
2013-11-05 13:16:13 +0000

    RBM library and Django successfully linked, training on basic data.

--- rbm_website/apps/rbm/urls.py
+++ rbm_website/apps/rbm/urls.py
@@ -1,1 +1,6 @@
 from django.conf.urls import patterns, include, url
+from rbm_website.apps.rbm import views
+
+urlpatterns = patterns('',
+        url(r'^$', views.index, name='index')
+        )
--- rbm_website/apps/rbm/views.py
+++ rbm_website/apps/rbm/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+import numpy as np
+from django.http import HttpResponse
+from rbm_website.libs.rbm import rbm
+
+def index(request):
+    m = rbm.RBM(6,4)
+    data = [[0,0,0,1,1,1],[1,1,1,0,0,0],[0,0,0,1,1,1]]
+    m.train(data)
+    (v, h) = m.regenerate([[0,0,0,1,1,1]])
+    return HttpResponse("It is working: " + np.array_str(v) + " - " + np.array_str(h))
+
