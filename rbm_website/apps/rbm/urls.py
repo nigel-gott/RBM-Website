95f9eacf1f8424463ed75ce19e3c9b6b7ac8b2e7
Nigel Gott <nigel.gott@gmail.com>
2013-11-05 13:48:58 +0000

    Basic version with index to view all RBMs, create RBM form and individual RBM view.

--- rbm_website/apps/rbm/urls.py
+++ rbm_website/apps/rbm/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, include, url
 from rbm_website.apps.rbm import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index')
+        url(r'^$', views.index, name='index'),
+        url(r'^create/$', views.create, name='create'),
+        url(r'^(?P<rbm_id>\d+)/$', views.view, name='view')
         )
--- rbm_website/apps/rbm/views.py
+++ rbm_website/apps/rbm/views.py
@@ -1,12 +1,21 @@
 # Create your views here.
 import numpy as np
 from django.http import HttpResponse
+from django.shortcuts import redirect, render, get_object_or_404
+
 from rbm_website.libs.rbm import rbm
+from rbm_website.apps.rbm.models import RBM 
 
 def index(request):
-    m = rbm.RBM(6,4)
-    data = [[0,0,0,1,1,1],[1,1,1,0,0,0],[0,0,0,1,1,1]]
-    m.train(data)
-    (v, h) = m.regenerate([[0,0,0,1,1,1]])
-    return HttpResponse("It is working: " + np.array_str(v) + " - " + np.array_str(h))
+    rbm_list = RBM.objects.all()
+    return render(request, 'rbm/index.html', {'rbm_list': rbm_list})
+
+def create(request):
+    new_rbm = RBM(name=request.POST['name'], matrix=request.POST['matrix'])
+    new_rbm.save()
+    return redirect('index')
+
+def view(request, rbm_id):
+    stored_rbm = get_object_or_404(RBM , pk=rbm_id)
+    return render(request, 'rbm/view.html', {'rbm': stored_rbm})
 
