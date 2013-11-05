8891b01ca4f3f9f55986ecfa8b571da7ef9aedb9
Nigel Gott <nigel.gott@gmail.com>
2013-11-05 20:05:16 +0000

    Now taking real training data, training and storing weights.

--- rbm_website/apps/rbm/views.py
+++ rbm_website/apps/rbm/views.py
@@ -11,11 +11,23 @@ def index(request):
     return render(request, 'rbm/index.html', {'rbm_list': rbm_list})
 
 def create(request):
-    new_rbm = RBM(name=request.POST['name'], matrix=request.POST['matrix'])
+    training_data = request.POST['matrix']
+    training_array = [map(int, x.split(',')) for x in training_data.split(';')]
+
+    trainer = rbm.RBM(
+            int(request.POST['visible']), 
+            int(request.POST['hidden']), 
+            float(request.POST['learning_rate']),
+            )
+    trainer.train(training_array)
+    matrix = list(np.array(trainer.weights).reshape(-1,))
+
+    new_rbm = RBM(name=request.POST['name'], matrix=matrix)
     new_rbm.save()
     return redirect('index')
 
 def view(request, rbm_id):
     stored_rbm = get_object_or_404(RBM , pk=rbm_id)
-    return render(request, 'rbm/view.html', {'rbm': stored_rbm})
+    matrix = str(stored_rbm.matrix)
+    return render(request, 'rbm/view.html', {'name': stored_rbm.name, 'matrix': matrix})
 
