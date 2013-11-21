35dc855fbe9578640a16f4513367fc27300468ed
Nigel Gott <nigel.gott@gmail.com>
2013-11-21 13:24:47 +0000

    Changed rbm_lib path back to fleurrette.

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ ln -s ../../hooks/pre-commit pre-commit
 ln -s ../../hooks/post-merge post-merge
 cd ../../
 echo "Cloning rbm lib.."
-git clone https://github.com/freefrag/RBM.git rbm_website/libs/rbm_lib
+git clone https://github.com/fleurette/RBM.git rbm_website/libs/rbm_lib
 
 echo "Finished, activate the virtual env with ./activate or 'source venv/bin/activate' or 'source venv/bin/activate.csh'"
