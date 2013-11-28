f791d463c6fd172d779c7ef0e4c140bf91b51e5e
jbcumming <james.cumming11@imperial.ac.uk>
2013-11-28 21:38:39 +0000

    Added the cleaning of the media folder to the install script

--- install.sh
+++ install.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 echo "Creating virtual env folder in venv..." 
 virtualenv venv
+
 echo "Activating the virtual env..."
 source venv/bin/activate
+
 echo "Installing all requirements..."
 pip install -r requirements.txt
+
 echo "Linking post commit and merge hooks..."
 cd .git/hooks
 ln -s ../../hooks/pre-commit pre-commit
 ln -s ../../hooks/post-merge post-merge
 cd ../../
-echo "Cloning rbm lib.."
+
+echo "Cloning rbm lib..."
 git clone https://github.com/fleurette/RBM.git rbm_website/libs/rbm_lib
 
-echo "Setting up database.."
+echo "Setting up database..."
 rm database.sqlite3
 python manage.py syncdb --noinput
+
+echo "Cleaning media folder..."
+cd  rbm_website/media
+mv info.txt ../
+rm -rf *
+mv ../info.txt .
 
 echo "Finished, activate the virtual env with ./activate or 'source venv/bin/activate' or 'source venv/bin/activate.csh'"
