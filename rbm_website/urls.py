6c50327a8a1c445926efa41069783e7ff8542720
Pierre Sutter <pierre.sutter@gmail.com>
2014-01-04 16:18:26 +0100

    updated faq section of the website

--- rbm_website/urls.py
+++ rbm_website/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'rbm_website.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^about/$', 'rbm_website.views.about', name='about'),
+    url(r'^faq/$', 'rbm_website.views.faq', name='faq'),
     url(r'^rbm/', include('rbm_website.apps.rbm.urls')),
     url(r'^users/', include('rbm_website.apps.users.urls')),
     url(r'^accounts/logout/', 'rbm_website.apps.users.views.user_logout'),
--- rbm_website/views.py
+++ rbm_website/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html', {})
 
-def about(request):
-    return render(request, 'about.html', {})
+def faq(request):
+    return render(request, 'faq.html', {})
 
 def terms(request):
     return render(request, 'terms.html', {})
