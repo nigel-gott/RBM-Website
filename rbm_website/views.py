117adbae5364d1884524b63c5537f0ebe05bed50
jbcumming <james.cumming11@imperial.ac.uk>
2014-01-22 13:53:06 +0000

    Fixed home page error

--- rbm_website/views.py
+++ rbm_website/views.py
@@ -4,12 +4,18 @@ from django.conf import settings
 
 # Loads the home page with the required DBN
 def home(request):
-    dbnset = DBNModel.objects.filter(id=settings.HOME_DBN)
-    if dbnset.count() == 0:
-        dbn = DBNModel.objects.all()[0]
+    dbnSet = DBNModel.objects.filter(id=settings.HOME_DBN)
+    has_dbn = True
+    dbn = None
+    if dbnSet.count() == 0:
+        dbnAll = DBNModel.objects.filter(private=False, trained=True)
+        if dbnAll.count() == 0:
+            has_dbn = False
+        else:
+            dbn = dbnAll[0]
     else:
-        dbn = dbnset[0]
-    return render(request, 'home.html', {'dbn' : dbn})
+        dbn = dbnSet[0]
+    return render(request, 'home.html', {'dbn' : dbn, 'has_dbn': has_dbn})
 
 # Loads the FAQ page
 def faq(request):
